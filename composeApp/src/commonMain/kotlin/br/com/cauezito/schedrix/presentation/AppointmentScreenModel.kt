016a1abb2eb0974028424a725c60c5c43a12113b
Cauê Santos <for.cauesantos@gmail.com>
2025-04-19 10:09:04 -0300

    feat: add functions to fetch times from API and change current month visualization

--- composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/AppointmentScreenModel.kt
+++ composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/AppointmentScreenModel.kt
@@ -1,9 +1,15 @@
 package br.com.cauezito.schedrix.presentation
 
 import br.com.cauezito.schedrix.domain.useCase.GetAvailableAppointmentTimesUseCase
+import br.com.cauezito.schedrix.extensions.DateExtensions
+import br.com.cauezito.schedrix.extensions.DateExtensions.formatStartAndEnd
 import br.com.cauezito.schedrix.extensions.DateExtensions.nextMonth
 import br.com.cauezito.schedrix.extensions.DateExtensions.previousMonth
+import br.com.cauezito.schedrix.presentation.mapper.asPresentation
+import br.com.cauezito.schedrix.presentation.mapper.mapToAppointmentCalendarDay
+import br.com.cauezito.schedrix.presentation.model.AppointmentDateTime
 import br.com.cauezito.schedrix.presentation.model.AppointmentMonth
+import br.com.cauezito.schedrix.presentation.model.AppointmentMonth.Companion.defineMonthPlaceholder
 import br.com.cauezito.schedrix.presentation.model.AppointmentMonth.NEXT
 import br.com.cauezito.schedrix.presentation.model.AppointmentMonth.PREVIOUS
 import cafe.adriel.voyager.core.model.ScreenModel
@@ -12,6 +18,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.number
 
 class AppointmentScreenModel(
     private val getAvailableTimes: GetAvailableAppointmentTimesUseCase
@@ -20,27 +27,63 @@ class AppointmentScreenModel(
     private val _state = MutableStateFlow(AppointmentState())
     val state: StateFlow<AppointmentState> = _state
 
-    internal fun fetchTimes(start: String, end: String) = screenModelScope.launch {
-        val availableTimes = getAvailableTimes(start = start, end = end)
+    private var appointments: List<AppointmentDateTime> = emptyList()
+    private var choseStartAndEndDates: Pair<String, String> = Pair("", "")
 
-        _state.value = AppointmentState(
-            appointments = availableTimes.availableAppointments,
-            interviewerName = availableTimes.interviewerName,
-            appointmentDuration = availableTimes.durationMinutes
+    init {
+        val todayDate = DateExtensions.getCurrentDate().date
+
+        choseStartAndEndDates = todayDate.formatStartAndEnd()
+        _state.value = _state.value.copy(currentMonthYear = todayDate)
+    }
+
+    internal fun fetchAvailableTimes() = screenModelScope.launch {
+        val monthPlaceHolder = _state.value.currentMonthYear.month.number.defineMonthPlaceholder()
+
+        val result = getAvailableTimes(
+            choseStartAndEndDates.first,
+            choseStartAndEndDates.second,
+            monthPlaceHolder
+        ).asPresentation()
+
+        appointments = result.availableAppointments
+
+        _state.value = _state.value.copy(
+            calendarDays = mapToAppointmentCalendarDay(
+                currentMonth = _state.value.currentMonthYear,
+                appointments = appointments,
+                selectedDate = _state.value.selectedDate
+            ),
+            showScreenLoading = false,
+            showContentLoading = false
         )
     }
 
     internal fun changeCurrentMonth(selectedMonth: AppointmentMonth) {
-        val currentDate = _state.value.currentMonthYear
-        val updatedDate = when (selectedMonth) {
-            PREVIOUS -> currentDate.previousMonth()
-            NEXT -> currentDate.nextMonth()
+        val currentMonth = _state.value.currentMonthYear
+        val updatedMonth = when (selectedMonth) {
+            PREVIOUS -> currentMonth.previousMonth()
+            NEXT -> currentMonth.nextMonth()
         }
 
-        _state.value = _state.value.copy(currentMonthYear = updatedDate)
+        _state.value = _state.value.copy(
+            currentMonthYear = updatedMonth,
+            showContentLoading = true
+        )
+
+        choseStartAndEndDates = updatedMonth.formatStartAndEnd()
+
+        fetchAvailableTimes()
     }
 
     internal fun changeSelectedDate(date: LocalDate) {
-        _state.value = _state.value.copy(selectedDate = date)
+        _state.value = _state.value.copy(
+            selectedDate = date,
+            calendarDays = mapToAppointmentCalendarDay(
+                currentMonth = _state.value.currentMonthYear,
+                appointments = appointments,
+                selectedDate = date
+            )
+        )
     }
 }
