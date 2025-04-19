2ceaa62b33637938ec46e60b60578024d572f73a
Cauê Santos <for.cauesantos@gmail.com>
2025-04-19 10:04:37 -0300

    feat: create CustomAnimation function

---
+++ composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/components/shared/CustomAnimation.kt
@@ -0,0 +1,32 @@
+package br.com.cauezito.schedrix.presentation.components.shared
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import io.github.alexzhirkevich.compottie.Compottie
+import io.github.alexzhirkevich.compottie.LottieCompositionSpec
+import io.github.alexzhirkevich.compottie.rememberLottieComposition
+import io.github.alexzhirkevich.compottie.rememberLottiePainter
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import schedrix.composeapp.generated.resources.Res
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+internal fun CustomAnimation(
+    animationPath: String = "files/schedrix_loading.json",
+    contentDescription: String = "Loading in progress! Wait a moment."
+) {
+    val composition by rememberLottieComposition {
+        LottieCompositionSpec.JsonString(
+            Res.readBytes(animationPath).decodeToString()
+        )
+    }
+
+    Image(
+        painter = rememberLottiePainter(
+            composition = composition,
+            iterations = Compottie.IterateForever
+        ),
+        contentDescription = contentDescription
+    )
+}
