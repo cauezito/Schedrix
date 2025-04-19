416be23364dcb3553d23f3711a57075241f71425
Cauê Santos <for.cauesantos@gmail.com>
2025-04-19 19:14:54 -0300

    feat: create Dimens object to handle all app dimension values

---
+++ composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/ui/tokens/Dimens.kt
@@ -0,0 +1,42 @@
+package br.com.cauezito.schedrix.ui.tokens
+
+import androidx.compose.ui.unit.dp
+
+internal object Dimens {
+    val dimens_1 = 1.dp
+    val dimens_2 = 2.dp
+    val dimens_3 = 3.dp
+    val dimens_4 = 4.dp
+    val dimens_6 = 6.dp
+    val dimens_8 = 8.dp
+    val dimens_12 = 12.dp
+    val dimens_14 = 14.dp
+    val dimens_16 = 16.dp
+    val dimens_20 = 20.dp
+    val dimens_24 = 24.dp
+    val dimens_28 = 28.dp
+    val dimens_32 = 32.dp
+    val dimens_40 = 40.dp
+    val dimens_44 = 44.dp
+    val dimens_46 = 46.dp
+    val dimens_58 = 58.dp
+    val dimens_60 = 60.dp
+    val dimens_62 = 62.dp
+    val dimens_78 = 78.dp
+    val dimens_100 = 100.dp
+    val dimens_350 = 350.dp
+}
+
+internal object Weight {
+    const val X_SMALL = 0.3f
+    const val X_SMALL2 = 0.6f
+    const val SMALL = 1f
+}
+
+internal object Numbers {
+    const val ZERO = 0
+    const val ONE = 1
+    const val TWO = 2
+    const val THREE = 3
+    const val SEVEN = 7
+}
