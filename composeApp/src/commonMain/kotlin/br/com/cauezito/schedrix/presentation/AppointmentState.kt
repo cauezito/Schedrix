b0ac8908ef87c1023de3709f1377459c5589f809
Cauê Santos <for.cauesantos@gmail.com>
2025-04-19 19:24:04 -0300

    feat: add date-related variables to state removing business logic from UI

--- composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/AppointmentState.kt
+++ composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/AppointmentState.kt
@@ -2,6 +2,7 @@ package br.com.cauezito.schedrix.presentation
 
 import br.com.cauezito.schedrix.extensions.DateExtensions
 import br.com.cauezito.schedrix.presentation.model.AppointmentCalendarDay
+import br.com.cauezito.schedrix.presentation.model.AppointmentDateTime
 import kotlinx.datetime.LocalDate
 
 data class AppointmentState(
@@ -11,5 +12,11 @@ data class AppointmentState(
     val currentMonthYear: LocalDate = DateExtensions.getCurrentDate().date,
     val interviewerName: String = "Cauê Santos",
     val selectedDate: LocalDate? = null,
+    val selectedDayOfWeekName: String = "",
+    val selectedMonthName: String = "",
+    val selectedDayOfMonthName: String = "",
+    val selectedFormattedTimes: List<String> = emptyList(),
+    val currentTimezone: String = "",
+    val selectedDateTimes: List<AppointmentDateTime> = emptyList(),
     val calendarDays: List<AppointmentCalendarDay> = emptyList()
 )
