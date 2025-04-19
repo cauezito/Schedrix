0e16c29398aa78e8cfbae76c89bdde841cee09ce
Cauê Santos <for.cauesantos@gmail.com>
2025-04-19 10:08:25 -0300

    refactor: remove unnecessary parameters from AppointmentPresentation

--- composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/model/AppointmentPresentation.kt
+++ composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/model/AppointmentPresentation.kt
@@ -3,12 +3,5 @@ package br.com.cauezito.schedrix.presentation.model
 import br.com.cauezito.schedrix.extensions.DateExtensions
 import kotlinx.datetime.LocalDateTime
 
-data class AppointmentPresentation(
-    val availableAppointments: List<AppointmentDateTime> = listOf(),
-    val interviewerName: String = "Cauê Santos",
-    val durationMinutes: String = "50"
-)
-
-data class AppointmentDateTime(
-    val availableAppointmentDateTime: LocalDateTime = DateExtensions.getCurrentDate()
-)
+data class AppointmentPresentation(val availableAppointments: List<AppointmentDateTime> = listOf())
+data class AppointmentDateTime(val availableAppointmentDateTime: LocalDateTime = DateExtensions.getCurrentDate())
