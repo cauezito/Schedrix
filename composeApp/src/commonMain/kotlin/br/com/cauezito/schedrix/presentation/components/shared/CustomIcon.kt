268204eb7472a37943dc54be3032e540b2815d18
Cauê Santos <for.cauesantos@gmail.com>
2025-04-19 10:12:05 -0300

    feat: create a generic function to render Icon

---
+++ composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/components/shared/CustomIcon.kt
@@ -0,0 +1,36 @@
+package br.com.cauezito.schedrix.presentation.components.shared
+
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun <T> CustomIcon(
+    imageVector: ImageVector,
+    contentDescription: String,
+    rippleHighlight: Boolean = false,
+    actionParam: T,
+    onAction: (T) -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Icon(
+        imageVector = imageVector,
+        contentDescription = contentDescription,
+        modifier = Modifier
+            .clickable(
+                interactionSource = interactionSource,
+                indication = if (rippleHighlight) LocalIndication.current else null
+            ) {
+                onAction(actionParam)
+            }
+            .padding(8.dp)
+    )
+}
