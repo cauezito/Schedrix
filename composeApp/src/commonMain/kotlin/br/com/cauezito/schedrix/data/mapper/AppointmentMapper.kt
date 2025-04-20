01b79cb9646eff89d7d19533ab2efeefedc84f2c
Cauê Santos <for.cauesantos@gmail.com>
2025-04-20 11:59:03 -0300

    refactor: filter only the valid dates

--- composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/data/mapper/AppointmentMapper.kt
+++ composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/data/mapper/AppointmentMapper.kt
@@ -2,7 +2,19 @@ package br.com.cauezito.schedrix.data.mapper
 
 import br.com.cauezito.schedrix.data.remote.model.AppointmentResponse
 import br.com.cauezito.schedrix.domain.model.Appointment
+import kotlinx.datetime.Instant
 
 internal object AppointmentMapper {
-    internal fun AppointmentResponse.asDomain() = Appointment(availableTimes = data.availableTimes.map { it })
+    internal fun AppointmentResponse.asDomain() = Appointment(
+        availableTimes = data.availableTimes.filter { it.isValidIsoDateTime() }
+    )
+
+    private fun String.isValidIsoDateTime(): Boolean {
+        return try {
+            Instant.parse(this)
+            true
+        } catch (_: Exception) {
+            false
+        }
+    }
 }
