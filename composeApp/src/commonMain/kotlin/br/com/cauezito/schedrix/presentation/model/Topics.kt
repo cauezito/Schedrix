3d8b92744c7bd7cc328f89ffb6981544e183fa37
Cauê Santos <for.cauesantos@gmail.com>
2025-04-20 18:10:19 -0300

    refactor: move topics list to a separate file

---
+++ composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/model/Topics.kt
@@ -0,0 +1,22 @@
+package br.com.cauezito.schedrix.presentation.model
+
+internal val topics = listOf(
+    "programming",
+    "video games",
+    "job offers",
+    "book recommendations",
+    "wellness",
+    "mental health",
+    "investments",
+    "travel experiences",
+    "productivity hacks",
+    "career growth",
+    "language learning",
+    "healthy habits",
+    "mindfulness",
+    "gym",
+    "book recommendations",
+    "coding challenges",
+    "public speaking",
+    "UX design"
+)
