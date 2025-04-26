9c667be17c2beffb5686e478ad44f040322fb8cf
Cauê Santos <caue.pereira@stone.com.br>
2025-04-25 22:58:22 -0300

    feat: add timezones list

---
+++ composeApp/src/commonMain/kotlin/br/com/cauezito/schedrix/presentation/model/Timezones.kt
@@ -0,0 +1,14 @@
+package br.com.cauezito.schedrix.presentation.model
+
+internal val timezones = listOf(
+    "America/New_York",
+    "Europe/London",
+    "Asia/Tokyo",
+    "Australia/Sydney",
+    "America/Los_Angeles",
+    "Europe/Paris",
+    "Asia/Kolkata",
+    "America/Sao_Paulo",
+    "Africa/Johannesburg",
+    "Pacific/Auckland"
+)
